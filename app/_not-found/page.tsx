69ebef445387e6032a05dc6a0cc4ac5828faafcb
machaeilaloumma-web <machaeil.aloumma@gmail.com>
2025-11-07 20:46:14 +0100

    Update page.tsx

--- app/_not-found/page.tsx
+++ app/_not-found/page.tsx
@@ -1,12 +1,8 @@
-import React from 'react';
-
 export default function NotFound() {
   return (
-    <html>
-      <body>
-        <h1>404 — Not Found</h1>
-        <p>صفحة بسيطة لاختبار البناء (build debug).</p>
-      </body>
-    </html>
+    <>
+      <h1>404 – Not Found</h1>
+      <p>صفحة بسيطة لاختبار البناء (build debug).</p>
+    </>
   );
 }
